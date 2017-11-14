dbea831dafeae341217b964837b07937ecd971eb
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-14 13:33:56 -0600

    Passport configuration-end. OAuth complete

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-appp.use(
+app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 

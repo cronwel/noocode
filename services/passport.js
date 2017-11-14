7d9adfd90b0c3a06b9b92b4d67fb8ce44de3fc02
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-14 15:41:39 -0600

    passport and prod update

--- services/passport.js
+++ services/passport.js
@@ -18,13 +18,13 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientId,
+      clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ GoogleId: profile.id }).then(existingUser => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           done(null, existingUser);
         } else {

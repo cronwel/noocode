97f6aa8358f1fab9a1f8ff6799f9800d2fc6e0c1
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-21 18:56:18 -0600

    login procedures complete, logout procedures start

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div>
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
       <div className="container">
         <BrowserRouter>
           <div>
@@ -19,8 +24,8 @@ const App = () => {
           </div>
         </BrowserRouter>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Log Out</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
@@ -8,15 +27,15 @@ class Header extends Component {
           <a href="" className="left brand-logo">
             NooCode
           </a>
-          <ul className="right">
-            <li>
-              <a href="">Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);

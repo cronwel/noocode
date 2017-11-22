df19a5b4fdd3841e8b9df21681dc1343e4d32482
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-21 21:32:21 -0600

    installion of stripe

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Whoa!!!!!!!!!!!!!!!!!!!</h1>
+      <h4>This is the landing page SON!</h4>
+    </div>
+  );
+};
+
+export default Landing;

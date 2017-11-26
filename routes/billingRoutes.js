a8c19f1645b58cd830f430cb6737ed43cdb1c035
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-25 19:06:34 -0600

    middleware configurations, routes

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,11 +3,17 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'Log in to add credits' });
+    }
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: '$5.00 for 5 credits',
       source: req.body.id
     });
+    req.user.credit += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };

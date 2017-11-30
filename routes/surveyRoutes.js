8c24b6157a517b8e0fea563fdee75240d041d1ef
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-30 15:00:43 -0600

    Mailer setup, configuration

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -19,5 +19,6 @@ module.exports = app => {
       dateSent: Date.now()
     });
     const mailer = new Mailer(survey, surveyTemplate(survey));
+    await mailer.send();
   });
 };
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,23 @@
 module.exports = survey => {
-  return '<div>' + survey.body + '</div>';
+  return `
+    <html>
+      <body>
+        <div style='text-align:center;'>
+          <h3>Your input matters!</h3>
+          <p>
+            Help us decide our next big feature
+          </p>
+          <p>
+          ${survey.body}
+          </p>
+          <div>
+            <a href="http://localhost:3000">Yes</a>
+          </div>
+          <div>
+            <a href="http://localhost:3000">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
 };

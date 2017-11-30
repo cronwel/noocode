e507e3010ecc81d9bf8795f4459e1c1193c3b9f3
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-30 17:32:26 -0600

    Survey api setup complete

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,6 +7,9 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('You voted?!!! That is democracry son!');
+  });
   app.post('api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -19,6 +22,16 @@ module.exports = app => {
       dateSent: Date.now()
     });
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    await mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.users.credit -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };

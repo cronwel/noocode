ac29849e4417c75e724c1e3c80b3204f2bd6ba28
Cronwel Noel Irias <noelirias@gmail.com>
2017-11-30 18:39:24 -0600

    Redux-form installed. redux-form initial setup

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Surveynew extends Component {
+  render() {
+    return <div>This is where the new survey form will be</div>;
+  }
+}
+
+export default SurvveyNew;

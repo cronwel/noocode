bc5aeb9008df9859ffa3087724d30b442bc9c896
Cronwel Noel Irias <noelirias@gmail.com>
2017-12-01 10:17:50 -0600

    Survey form review setup

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -45,7 +45,9 @@ class SurveyForm extends Component {
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form
+          onSubmit={this.props.handleSubmit(() => this.props.onSurveySubmit())}
+        >
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat left white-text">
             Cancel
---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SurveyReview = () => {
+  return (
+    <div>
+      <h5>Review before sending</h5>
+    </div>
+  );
+};
+
+export default SurveyReview;

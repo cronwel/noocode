d39566ca08100e6361a3f27b42142ffc88d3295d
cronwel <noelirias@gmail.com>
2019-08-12 22:12:40 -0500

    Update to landing page Update to landing page only.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,12 @@ import React from 'react';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Whoa!!!!!!!!!!!!!!!!!!!</h1>
-      <h4>This is the landing page SON!</h4>
+      <h1>Welcome</h1>
+      <h4>Send emails to your audience</h4>
+      <h4>Up to 100 targeted emails</h4>
+      <h4>Flat Fee of $5.00USD</h4>
+      <h4>Start Now!!!</h4>
+
     </div>
   );
 };
